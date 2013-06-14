5a75135abb53dfe357745f8e1e9f4b034d8a4994
xuehua <txh_723@163.com>
2013-06-14 22:42:13 +0800

    delete useless files.

--- build.bat
+++
@@ -1,1 +0,0 @@
-tools\nant\nant.exe %1 -D:args0=%2 -D:args1=%3 -D:runtime=dotnet
--- nuget.install.bat
+++
@@ -1,4 +0,0 @@
-echo off
-echo install
-@forfiles /s /m packages.config /c "cmd /c %1\nuget install @file -o %2 -source http://nuget.org/api/v2"
-echo on
