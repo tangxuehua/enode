757194e2fa05fe330191789e03a84864fa2ecea1
xuehua <txh_723@163.com>
2013-08-04 21:22:10 +0800

    Recover the Note sample code.

--- src/Samples/NoteSample/Program.cs
+++ src/Samples/NoteSample/Program.cs
@@ -11,10 +11,10 @@ namespace NoteSample
     {
         static void Main(string[] args)
         {
-            //new ENodeFrameworkUnitTestInitializer().Initialize();
+            new ENodeFrameworkUnitTestInitializer().Initialize();
 
             //如果要使用Sql或MongoDB来持久化，请用下面相应的语句来初始化
-            new ENodeFrameworkSqlInitializer().Initialize();
+            //new ENodeFrameworkSqlInitializer().Initialize();
             //new ENodeFrameworkMongoInitializer().Initialize();
 
             //如果要使用Redis来作为内存缓存，请用下面相应的语句来初始化
@@ -25,12 +25,10 @@ namespace NoteSample
 
             var noteId = Guid.NewGuid();
 
-            //var command1 = new CreateNote { NoteId = noteId, Title = "Sample Note" };
-            //var command2 = new ChangeNoteTitle { NoteId = noteId, Title = "Modified Note" };
+            var command1 = new CreateNote { NoteId = noteId, Title = "Sample Note" };
+            var command2 = new ChangeNoteTitle { NoteId = noteId, Title = "Modified Note" };
 
-            //commandService.Send(command1, (result) => commandService.Send(command2));
-
-            //commandService.Send(command1);
+            commandService.Send(command1, (result) => commandService.Send(command2));
 
             Thread.Sleep(1000);
             Console.WriteLine("Press Enter to exit...");
