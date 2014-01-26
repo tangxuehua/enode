8194934b9da79efbef155738617ef21018d06274
tangxuehua <txh_723@163.com>
2014-01-26 22:56:02 +0800

    refactory code.

--- src/Extensions/ENode.EQueue/Eventing/EventConsumer.cs
+++ src/Extensions/ENode.EQueue/Eventing/EventConsumer.cs
@@ -73,5 +73,22 @@ namespace ENode.EQueue
 
             return new EventStream(data.AggregateRootId, data.AggregateRootName, data.Version, data.CommandId, data.Timestamp, events);
         }
+
+        class EventProcessContext : IEventProcessContext
+        {
+            public Action<EventStream, QueueMessage> EventProcessedAction { get; private set; }
+            public QueueMessage QueueMessage { get; private set; }
+
+            public EventProcessContext(QueueMessage queueMessage, Action<EventStream, QueueMessage> eventProcessedAction)
+            {
+                QueueMessage = queueMessage;
+                EventProcessedAction = eventProcessedAction;
+            }
+
+            public void OnEventProcessed(EventStream eventStream)
+            {
+                EventProcessedAction(eventStream, QueueMessage);
+            }
+        }
     }
 }
