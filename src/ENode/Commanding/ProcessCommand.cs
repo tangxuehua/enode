adf0d7772da5c21d53acc6f580acd86b3e16f3aa
tangxuehua <txh_723@163.com>
2014-02-10 17:57:42 +0800

    add a new file.

---
+++ src/ENode/Commanding/ProcessCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ENode.Commanding
+{
+    /// <summary>Represents an abstract process command which associated with a specific business process.
+    /// </summary>
+    [Serializable]
+    public abstract class ProcessCommand<TAggregateRootId> : Command<TAggregateRootId>, IProcessCommand
+    {
+        public abstract string ProcessId { get; }
+
+        /// <summary>Parameterized constructor.
+        /// </summary>
+        /// <param name="aggregateRootId"></param>
+        protected ProcessCommand(TAggregateRootId aggregateRootId) : base(aggregateRootId) { }
+        /// <summary>Parameterized constructor.
+        /// </summary>
+        /// <param name="aggregateRootId"></param>
+        /// <param name="retryCount"></param>
+        protected ProcessCommand(TAggregateRootId aggregateRootId, int retryCount) : base(aggregateRootId, retryCount)
+        {
+        }
+
+        /// <summary>Returns the command type name.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+}
