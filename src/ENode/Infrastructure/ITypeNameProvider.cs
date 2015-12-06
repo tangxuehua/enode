09e4ccb0c6912d9841b68bffcfa5d1399b0f4369
tangxuehua <txh_723@163.com>
2015-12-06 18:24:35 +0800

    添加漏掉的文件

---
+++ src/ENode/Infrastructure/ITypeNameProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ENode.Infrastructure.Impl;
+
+namespace ENode.Infrastructure
+{
+    /// <summary>Represents a provider to provide the type and type name mapping information.
+    /// </summary>
+    public interface ITypeNameProvider
+    {
+        /// <summary>Get the type name of the given type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        string GetTypeName(Type type);
+        /// <summary>Get the type of the given type name.
+        /// </summary>
+        /// <param name="typeName"></param>
+        /// <returns></returns>
+        Type GetType(string typeName);
+    }
+}
---
+++ src/ENode/Infrastructure/Impl/DefaultTypeNameProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace ENode.Infrastructure.Impl
+{
+    public class DefaultTypeNameProvider : ITypeNameProvider, IAssemblyInitializer
+    {
+        private Assembly[] _asemblies;
+        private readonly ConcurrentDictionary<string, Type> _dict = new ConcurrentDictionary<string, Type>();
+
+        public Type GetType(string typeName)
+        {
+            Type t;
+            if (_dict.TryGetValue(typeName, out t))
+            {
+                return t;
+            }
+            foreach (var assembly in _asemblies)
+            {
+                var type = assembly.GetType(typeName);
+                if (type != null)
+                {
+                    _dict.TryAdd(typeName, type);
+                    return type;
+                }
+            }
+            return null;
+        }
+        public string GetTypeName(Type type)
+        {
+            return type.FullName;
+        }
+        public void Initialize(params Assembly[] assemblies)
+        {
+            _asemblies = assemblies;
+        }
+    }
+}
