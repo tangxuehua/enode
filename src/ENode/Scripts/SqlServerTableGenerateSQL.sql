7012a07e96c4e607e5eca4b747018c024c68fcb0
tangxuehua <txh_723@163.com>
2017-08-25 06:46:18 +0800

    SQL初始化脚本中去掉Command表

--- src/ENode/Scripts/SqlServerTableGenerateSQL.sql
+++ src/ENode/Scripts/SqlServerTableGenerateSQL.sql
@@ -2,18 +2,6 @@ CREATE DATABASE [ENode]
 GO
 USE [ENode]
 GO
-CREATE TABLE [dbo].[Command] (
-    [Sequence]                BIGINT IDENTITY (1, 1) NOT NULL,
-    [CommandId]               NVARCHAR (36)          NOT NULL,
-    [CreatedOn]               DATETIME               NOT NULL,
-    [AggregateRootId]         NVARCHAR (36)          NULL,
-    [MessagePayload]          NVARCHAR (MAX)         NULL,
-    [MessageTypeName]         NVARCHAR (256)         NULL,
-    CONSTRAINT [PK_Command] PRIMARY KEY CLUSTERED ([Sequence] ASC)
-)
-GO
-CREATE UNIQUE INDEX [IX_Command_CommandId] ON [dbo].[Command] ([CommandId] ASC)
-GO
 
 CREATE TABLE [dbo].[EventStream] (
     [Sequence]              BIGINT IDENTITY (1, 1) NOT NULL,
