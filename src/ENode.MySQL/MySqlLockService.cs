21a1ef55dac50a0a046af17fb878a8b4d038ddf9
Mr-Daomi <nowme@Outlook.com>
2018-01-07 01:17:15 +0800

    Update MySqlLockService.cs
    修正无法连接mysql数据库

--- src/ENode.MySQL/MySqlLockService.cs
+++ src/ENode.MySQL/MySqlLockService.cs
@@ -6,6 +6,7 @@ using Dapper;
 using ECommon.Dapper;
 using ECommon.Utilities;
 using ENode.Infrastructure;
+using MySql.Data.MySqlClient;
 
 namespace ENode.MySQL
 {
@@ -69,9 +70,9 @@ namespace ENode.MySQL
             var sql = string.Format(_lockKeySqlFormat, key);
             transaction.Connection.Query(sql, transaction: transaction);
         }
-        private SqlConnection GetConnection()
+        private MySqlConnection GetConnection()
         {
-            return new SqlConnection(_connectionString);
+            return new MySqlConnection(_connectionString);
         }
     }
 }
