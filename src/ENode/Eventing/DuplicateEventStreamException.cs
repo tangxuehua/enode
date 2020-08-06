1f027dfcab096b509d73a9fd4912e4964b8d891a
tangxuehua <txh_723@163.com>
2020-08-06 22:46:54 +0800

    升级版本号

---
+++ src/ENode/Eventing/DuplicateEventStreamException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ENode.Eventing
+{
+    public class DuplicateEventStreamException : Exception
+    {
+        private const string ExceptionMessage = "Aggregate root [type={0},id={1}] event stream already exist in the EventCommittingContextMailBox, eventStreamId: {2}";
+
+        public DomainEventStream DomainEventStream { get; set; }
+
+        /// <summary>Parameterized constructor.
+        /// </summary>
+        public DuplicateEventStreamException(DomainEventStream domainEventStream) : base(string.Format(ExceptionMessage, domainEventStream.AggregateRootTypeName, domainEventStream.AggregateRootId, domainEventStream.Version))
+        {
+            DomainEventStream = domainEventStream;
+        }
+    }
+}
